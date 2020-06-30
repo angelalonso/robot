357ccc58e884c166eaef808bd0bb43e7daf777d8
Angel Alonso Fonseca <angel.fonseca@deliveryhero.com>
2020-06-30 14:56:26 +0200

    loading vars from dotenv

--- deployer/src/main.rs
+++ deployer/src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
 use std::process::{Command, Stdio};
-use std::io::{self, Write};
+//use std::io::{self, Write};
+use std::io::{self};
+use dotenv;
 
 // TODO
 //
@@ -16,7 +18,7 @@ fn scp(file_to_scp: &str, destination: &str) -> io::Result<()> {
         .spawn()?;
 
     {
-        let child_stdin = child.stdin.as_mut().unwrap();
+        let _child_stdin = child.stdin.as_mut().unwrap();
     }
 
     let output = child.wait_with_output()?;
@@ -27,7 +29,16 @@ fn scp(file_to_scp: &str, destination: &str) -> io::Result<()> {
     // TODO: catch output = Output { status: ExitStatus(ExitStatus(256)), stdout: "", stderr: "" }
 }
 
+fn print_env_var(key: &str) {
+    // Accessing an env var
+    match env::var(key) {
+        Ok(val) => println!("{}: {}", key, val),
+        Err(e) => println!("Couldn't print env var {}: {}", key, e),
+    }
+}
+
 fn main() {
+    dotenv::dotenv().ok();
     let mut args: Vec<String> = env::args().collect();
     args.drain(0..1);
 
@@ -35,6 +46,24 @@ fn main() {
 
     for arg in args.iter() {
         println!("Copying {}", arg);
-        scp(&arg, login_and_destination);
+        let retest = scp(&arg, login_and_destination);
     }
+
+    println!("Listing all env vars:");
+    for (key, val) in env::vars() {
+        println!("{}: {}", key, val);
+    }
+    let key = "TESTVAR";
+    print_env_var(key);
+     println!("Setting env var {}", key);
+     // Setting an env var for the current process
+     env::set_var(key, "8080");
+ 
+     print_env_var(key);
+ 
+     // Removing an env var for the current process
+     println!("Removing env var {}", key);
+     env::remove_var(key);
+ 
+     print_env_var(key);
 }
