ac2ad512012a4b75b18ed939b753da95febdaf92
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-07-11 17:15:42 +0200

    test

--- brain/src/main.rs
+++ brain/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{BufReader, BufRead};
 
 fn main() {
-    let f = File::open("/dev/ttyUSB0").expect("oh no");
+    let f = File::open("/dev/ttyUSB0").expect("oh noo");
     let mut f = BufReader::new(f);
     let mut read_buffer: Vec<u8> = Vec::new();
     f.read_until(b'V', &mut read_buffer).expect("reading from cursor won't fail");
