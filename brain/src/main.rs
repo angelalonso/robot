57584264a29650515f839aa478134a2999186619
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-07-11 14:17:19 +0200

    trying to print

--- brain/src/main.rs
+++ brain/src/main.rs
@@ -6,5 +6,6 @@ fn main() {
     let mut f = BufReader::new(f);
     let mut read_buffer: Vec<u8> = Vec::new();
     f.read_until(b'V', &mut read_buffer).expect("reading from cursor won't fail");
+    print!("{:?}", f);
 
 }
