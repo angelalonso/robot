ab70601d3f851e754af30baa7c1257e310e8bb4b
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-07-12 13:23:23 +0200

    example of reading through a file instead of serial, for tests

--- brain/src/main.rs
+++ brain/src/main.rs
@@ -1,58 +1,28 @@
-#![warn(rust_2018_idioms)]
+use std::env;
+use std::io;
 
-use std::{env, io, str};
-use tokio_util::codec::{Decoder, Encoder};
-use futures::stream::StreamExt;
+use logwatch::Watcher;
 
-use bytes::BytesMut;
+//-- Choose an implementation --
+use logwatch::PollWatcher as LogWatcher;
+//use logwatch::TailWatcher as LogWatcher;
 
-//const DEFAULT_TTY: &str = "/dev/tty7";
-const DEFAULT_TTY: &str = "/dev/ttyUSB0";
-
-struct LineCodec;
-
-impl Decoder for LineCodec {
-    type Item = String;
-    type Error = io::Error;
-
-    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        let newline = src.as_ref().iter().position(|b| *b == b'\n');
-        if let Some(n) = newline {
-            let line = src.split_to(n + 1);
-            return match str::from_utf8(line.as_ref()) {
-                Ok(s) => Ok(Some(s.to_string())),
-                Err(_) => Err(io::Error::new(io::ErrorKind::Other, "Invalid String")),
-            };
-        }
-        Ok(None)
-    }
-}
-
-impl Encoder for LineCodec {
-    type Item = String;
-    type Error = io::Error;
-
-    fn encode(&mut self, _item: Self::Item, _dst: &mut BytesMut) -> Result<(), Self::Error> {
-        Ok(())
-    }
-}
-
-#[tokio::main]
-async fn main() {
-    let mut args = env::args();
-    let tty_path = args.nth(1).unwrap_or_else(|| DEFAULT_TTY.into());
-
-    let settings = tokio_serial::SerialPortSettings::default();
-    let mut port = tokio_serial::Serial::from_path(tty_path, &settings).unwrap();
-
-    #[cfg(unix)]
-    port.set_exclusive(false)
-        .expect("Unable to set serial port exclusive to false");
-
-    let mut reader = LineCodec.framed(port);
-
-    while let Some(line_result) = reader.next().await {
-        let line = line_result.expect("Failed to read line");
+fn factory<'a>(count: &'a mut usize) -> Box<dyn FnMut(String) + 'a> {
+    let callback = move |line: String| {
+        *count += 1;
         println!("{}", line);
-    }
+    };
+    Box::new(callback)
+}
+
+fn main() -> Result<(), io::Error> {
+    let fpath = env::args()
+        .nth(1)
+        .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "usage: logwatch /path/to/log"))?;
+    let mut count = 0;
+    let mut watcher = LogWatcher::new(&fpath, 2000); // polling period = 2000 milliseconds
+    let callback = factory(&mut count);
+    watcher.register(callback);
+    watcher.watch();
+    Ok(())
 }
