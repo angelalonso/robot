c21a4926e2e07a3100a0a0313ab4ab06c9a2e66c
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-08-17 17:13:14 +0200

    using another prog

--- brain/src/main.rs
+++ brain/src/main.rs
@@ -10,7 +10,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         process::exit(1);
     });
     // Send the first file that triggers our ping-pong loop of messages to start.
-    let mut send_first = main_brain.sendfile_serial("../tests/000_blick_internal_led_seconds/000_blick_internal_led_seconds.ino.hex").unwrap_or_else(|err| {
+    //let mut send_first = main_brain.sendfile_serial("../tests/000_blick_internal_led_seconds/000_blick_internal_led_seconds.ino.hex").unwrap_or_else(|err| {
+    let mut send_first = main_brain.sendfile_serial("../tests/000_blick_internal_led_seconds_inverted/000_blick_internal_led_seconds_inverted.ino.hex").unwrap_or_else(|err| {
         eprintln!("Problem sending the first program to the Arduino: {}", err);
         process::exit(1);
     });
