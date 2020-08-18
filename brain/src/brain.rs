7eff668b694828acaae87eaf4a24a741989b66d1
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-08-18 16:11:14 +0200

    reading from serial

--- brain/src/brain.rs
+++ brain/src/brain.rs
@@ -241,8 +241,33 @@ impl Brain<'_> {
 
     /// This one represents the loop that reads several times
     pub fn read_msgs_serial(&mut self) -> Result<(), BrainDeadError> {
-        Err(BrainDeadError::ReadSerialError)
+        log(Some(&self.name), "D", "Waiting for data...");
+        loop {
+            let results = self.read_msg_serial();
+            //self.get_actions(&results.unwrap());
+        }
     }
+
+    #[tokio::main]
+    pub async fn read_msg_serial(&mut self) -> Result<(), BrainDeadError> {
+        log(Some(&self.name), "D", &format!("Reading from Serial Port {}", self.serialport));
+        //Err(BrainDeadError::EmptyError)
+        let settings = tokio_serial::SerialPortSettings::default();
+        let mut port = tokio_serial::Serial::from_path(self.serialport, &settings).unwrap();
+
+        #[cfg(unix)]
+        port.set_exclusive(false)
+            .expect("Unable to set serial port exclusive to false");
+
+        let mut reader = LineCodec.framed(port);
+
+        while let Some(line_result) = reader.next().await {
+            let line = line_result.expect("Failed to read line");
+            println!("{}", line);
+        }
+        Ok(())
+    }
+
 
     /// We actually need to read Serial,
     /// names apart, is this one the right one? or is it the next one?
