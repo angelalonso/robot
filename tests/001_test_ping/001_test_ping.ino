daa4288c16aa918ec5b8344d149f893b1584fcf6
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-08-21 20:10:06 +0200

    starting to make sense4

--- tests/001_test_ping/001_test_ping.ino
+++ tests/001_test_ping/001_test_ping.ino
@@ -7,14 +7,15 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  int count = 4;
+  int count = 10;
   while(count > 0 )                                   // repeat until count is no longer greater than zero
   {
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
     delay(50);                       // wait for half a second
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
     delay(950);                       // wait for half a second
-    Serial.println("ping");
+    Serial.println("not yet ping,");
     count = count-1;
   }
+  Serial.println("ping");
 }
