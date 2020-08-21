82dbbc05d252155b8ecdf4e8c6368768ce101ae4
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-08-21 20:39:39 +0200

    ardu loop

--- tests/001_test_pong/001_test_pong.ino
+++ tests/001_test_pong/001_test_pong.ino
@@ -2,31 +2,18 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
-  int i = 0;
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
-  delay(50);                       // wait for half a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
-  delay(950);                       // wait for half a second
-  Serial.println("a");
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
-  delay(50);                       // wait for half a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
-  delay(950);                       // wait for half a second
-  Serial.println("b");
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
-  delay(50);                       // wait for half a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
-  delay(950);                       // wait for half a second
-  Serial.println("c");
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
-  delay(50);                       // wait for half a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
-  delay(950);                       // wait for half a second
+  int j;
+  for ( j = 0; j < 10; j++) {
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
+    delay(50);                       // wait for half a second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
+    delay(950);                       // wait for half a second
 
+  }
   Serial.println("pong");
 }
 
 // the loop function runs over and over again forever
 void loop() {
-// put your main code here, to run repeatedly:
+// put your main code here, to run repeatedly
 }
