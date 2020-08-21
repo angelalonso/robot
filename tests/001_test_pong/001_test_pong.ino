09d4f8cf06861f47dd6a44ae31f51e25bc3f628b
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-08-21 20:28:49 +0200

    starting to make sense8

--- tests/001_test_pong/001_test_pong.ino
+++ tests/001_test_pong/001_test_pong.ino
@@ -2,11 +2,6 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
-
-}
-
-// the loop function runs over and over again forever
-void loop() {
   for (int j = 0; j < 10; j++) {
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
     delay(50);                       // wait for half a second
@@ -15,4 +10,9 @@ void loop() {
     Serial.println(j, DEC);
   }
   Serial.println("pong");
+}
+
+// the loop function runs over and over again forever
+void loop() {
+
 }
