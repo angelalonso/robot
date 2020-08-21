865fe6296264cdc6a2c27834158ff159bb22ef96
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-08-21 20:43:08 +0200

    ardu loop

--- tests/001_test_ping/001_test_ping.ino
+++ tests/001_test_ping/001_test_ping.ino
@@ -2,20 +2,18 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+  int j;
+  for ( j = 0; j < 10; j++) {
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED on 
+    delay(50);                       // wait for half a second
+    digitalWrite(LED_BUILTIN, HIGH);    // turn the LED off 
+    delay(950);                       // wait for half a second
 
+  }
+  Serial.println("ping");
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  int count = 10;
-  while(count > 0 )                                   // repeat until count is no longer greater than zero
-  {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
-    delay(50);                       // wait for half a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
-    delay(950);                       // wait for half a second
-    Serial.println("not yet ping,");
-    count = count-1;
-  }
-  Serial.println("ping");
+// put your main code here, to run repeatedly
 }
