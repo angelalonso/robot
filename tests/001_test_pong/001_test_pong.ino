f498a7461ff4abcf196cec8e944b5703d858c43c
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-08-21 20:32:47 +0200

    starting to make sense9

--- tests/001_test_pong/001_test_pong.ino
+++ tests/001_test_pong/001_test_pong.ino
@@ -2,17 +2,19 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
-  for (int j = 0; j < 10; j++) {
+  int j;
+  for ( j = 0; j < 4; j++) {
+    Serial.println(j);
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
     delay(50);                       // wait for half a second
     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
     delay(950);                       // wait for half a second
-    Serial.println(j, DEC);
+
   }
   Serial.println("pong");
 }
 
 // the loop function runs over and over again forever
 void loop() {
-
+// put your main code here, to run repeatedly:
 }
