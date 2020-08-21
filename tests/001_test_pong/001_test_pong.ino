5cde16e287e4a084a473f945f159d7961115f589
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-08-21 20:35:38 +0200

    ardu loop

--- tests/001_test_pong/001_test_pong.ino
+++ tests/001_test_pong/001_test_pong.ino
@@ -2,15 +2,23 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
-  int j;
-  for ( j = 0; j < 4; j++) {
-    Serial.println(j);
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
-    delay(50);                       // wait for half a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
-    delay(950);                       // wait for half a second
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
+  delay(50);                       // wait for half a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
+  delay(950);                       // wait for half a second
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
+  delay(50);                       // wait for half a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
+  delay(950);                       // wait for half a second
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
+  delay(50);                       // wait for half a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
+  delay(950);                       // wait for half a second
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
+  delay(50);                       // wait for half a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
+  delay(950);                       // wait for half a second
 
-  }
   Serial.println("pong");
 }
 
