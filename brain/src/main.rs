c8c023e5d6026b5723d6a270408c4dd89c83713d
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-08-28 22:38:41 +0200

    back

--- brain/src/main.rs
+++ brain/src/main.rs
@@ -15,7 +15,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         process::exit(1);
     });
     // Listening on Bus
-    //let mut _brain_thread = main_brain.read_loop();
-    main_brain.read();
+    let mut _brain_thread = main_brain.read_loop();
+    //main_brain.read();
     Ok(())
 }
