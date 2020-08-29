8e9657b829ff5790b207a5afb5c96cab0a3de130
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-08-29 16:12:50 +0200

    adding a deploy script

---
+++ brain/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source .env
+
+git checkout ${DEV_BRANCH}
+git add ${ARDUINO_FILES}
+git add src/
+git add Cargo.toml
+git commit -m "ready to test"
+git push origin ${DEV_BRANCH}
+
+
+${SSH_COMM} "cd robot/brain; pwd; git checkout ${DEV_BRANCH} && git pull && \
+  ${CARGO} run
+  "
