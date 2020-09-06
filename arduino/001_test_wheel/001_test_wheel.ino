ddd0df70c25561ccdecb04fb1afdce802c990300
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-09-06 13:19:50 +0200

    ready to test

--- arduino/001_test_wheel/001_test_wheel.ino
+++ arduino/001_test_wheel/001_test_wheel.ino
@@ -18,6 +18,7 @@ void setup() {
     Serial.print("LOG: j is ");  //messages starting with LOG: will be read but not taken as result
     Serial.println(j);
   }
+  delay(50); // always in front of a serial println
   Serial.println("Wheel Finished");
 }
 void loop() {
