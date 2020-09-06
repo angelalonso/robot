7f6fe27c9d2b207e35f8903dc0202125b64fe84c
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-09-06 13:28:16 +0200

    ready to test

--- arduino/000_ready/000_ready/000_ready.ino
+++ arduino/000_ready/000_ready/000_ready.ino
@@ -7,13 +7,13 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
 // put your main code here, to run repeatedly
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on 
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
   delay(50);                       
-  digitalWrite(LED_BUILTIN, HIGH);    // turn the LED off 
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
   delay(100);     
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED on 
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on 
   delay(50);                       
-  digitalWrite(LED_BUILTIN, HIGH);    // turn the LED off 
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off 
   delay(800);                      
   Serial.println("LOG: Ready");  //messages starting with LOG: will be read but not taken as result
 }
