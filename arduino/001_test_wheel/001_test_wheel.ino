0f08daa6d4c99c48c48a527f784b8909ceefb887
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-09-06 17:13:16 +0200

    ready to test

--- arduino/001_test_wheel/001_test_wheel.ino
+++ arduino/001_test_wheel/001_test_wheel.ino
@@ -1,19 +1,28 @@
 
 void setup() {
-  #define enA 9
-  #define in1 7
-  #define in2 8
+  #define inA1 4
+  #define inA2 5
+  #define enA 6
+  #define inB1 7
+  #define inB2 8
+  #define enB 9
   
   pinMode(enA, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
+  pinMode(inA1, OUTPUT);
+  pinMode(inA2, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(inB1, OUTPUT);
+  pinMode(inB2, OUTPUT);
   // Set initial rotation direction
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
+  digitalWrite(inA1, HIGH);
+  digitalWrite(inA2, LOW);
+  digitalWrite(inB1, HIGH);
+  digitalWrite(inB2, LOW);
   Serial.begin(9600);
   int j;
   for ( j = 0; j < 256; j++) {
     analogWrite(enA, j);
+    analogWrite(enB, j);
     delay(50);                                            
     Serial.print("LOG: j is ");  //messages starting with LOG: will be read but not taken as result
     Serial.println(j);
