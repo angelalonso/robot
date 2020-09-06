61ed5582fb8b36a954b69bd840132558cc19d38f
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-09-06 21:33:19 +0200

    ready to test

--- arduino/001_test_wheel/001_test_wheel.ino
+++ arduino/001_test_wheel/001_test_wheel.ino
@@ -6,6 +6,7 @@ void setup() {
   #define inB1 7
   #define inB2 8
   #define enB 9
+  #define speed 255
   
   pinMode(enA, OUTPUT);
   pinMode(inA1, OUTPUT);
@@ -19,17 +20,53 @@ void setup() {
   digitalWrite(inB1, HIGH);
   digitalWrite(inB2, LOW);
   Serial.begin(9600);
-  int j;
-  for ( j = 0; j < 256; j++) {
-    analogWrite(enA, j);
-    analogWrite(enB, j);
-    delay(50);                                            
-    Serial.print("LOG: j is ");  //messages starting with LOG: will be read but not taken as result
-    Serial.println(j);
-  }
-  delay(50); // always in front of a serial println
-  Serial.println("Wheel Finished");
 }
 void loop() {
- 
+  int i;
+  for ( i = 0; i < 2; i++) {
+    delay(1000);     
+    Serial.println("LOG: Un,");
+    delay(1000);
+    Serial.println("LOG: dos,");
+    delay(1000);
+    Serial.println("LOG: tres!");
+    delay(1000);
+    Serial.println("LOG: Un pasito palante, Maria!");
+    digitalWrite(inA1, HIGH);
+    digitalWrite(inA2, LOW);
+    digitalWrite(inB1, HIGH);
+    digitalWrite(inB2, LOW);
+    analogWrite(enA, speed);
+    analogWrite(enB, speed);
+    delay(1000);
+    analogWrite(enA, 0);
+    analogWrite(enB, 0);
+    Serial.println("LOG: Un,");
+    delay(1000);
+    Serial.println("LOG: dos,");
+    delay(1000);
+    Serial.println("LOG: tres!");
+    delay(1000);
+    Serial.println("LOG: Un pasito patras...");
+    digitalWrite(inA1, LOW);
+    digitalWrite(inA2, HIGH);
+    digitalWrite(inB1, LOW);
+    digitalWrite(inB2, HIGH);
+    analogWrite(enA, speed);
+    analogWrite(enB, speed);
+    delay(1000);
+    analogWrite(enA, 0);
+    analogWrite(enB, 0);
+  }
+  Serial.println("LOG: Aunque me muera ahora!");
+  digitalWrite(inA1, LOW);
+  digitalWrite(inA2, LOW);
+  digitalWrite(inB1, LOW);
+  digitalWrite(inB2, HIGH);
+  analogWrite(enB, speed);
+  delay(2000);
+  analogWrite(enA, 0);
+  analogWrite(enB, 0);
+  delay(50); // always in front of a serial println
+  Serial.println("Wheel Finished");
 }
