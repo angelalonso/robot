f66d667bcf0e825e6ae659385a48dc9118d64d66
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-09-15 15:50:14 +0200

    ready to test

--- arduino/001_test_proximity/001_test_proximity/001_test_proximity.ino
+++ arduino/001_test_proximity/001_test_proximity/001_test_proximity.ino
@@ -1,10 +1,3 @@
-const int motorInA1Pin = 4;
-const int motorInA2Pin = 5;
-const int motorEnAPin = 6;
-const int motorInB1Pin = 7;
-const int motorInB2Pin = 8;
-const int motorEnBPin = 9;
-const int motorSpeed = 255;
 const int proxTriggerPin = 10;
 const int proxEchoPin = 11;
 // vars
@@ -12,17 +5,6 @@ long duration;
 int distance;
 
 void setup() {
-  pinMode(motorEnAPin, OUTPUT);
-  pinMode(motorInA1Pin, OUTPUT);
-  pinMode(motorInA2Pin, OUTPUT);
-  pinMode(motorEnBPin, OUTPUT);
-  pinMode(motorInB1Pin, OUTPUT);
-  pinMode(motorInB2Pin, OUTPUT);
-  // Set initial rotation direction
-  digitalWrite(motorInA1Pin, LOW);
-  digitalWrite(motorInA2Pin, HIGH);
-  digitalWrite(motorInB1Pin, LOW);
-  digitalWrite(motorInB2Pin, HIGH);
   pinMode(proxTriggerPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(proxEchoPin, INPUT); // Sets the echoPin as an Input
   Serial.begin(9600); // Starts the serial communication
@@ -43,13 +25,12 @@ void loop() {
   distance = duration*0.034/2;
   Serial.print("LOG: Distance -> ");
   Serial.println(distance);
-  if (distance < 10) {
-    Serial.print("LOG: TOO CLOSE!");
-      analogWrite(motorEnAPin, motorSpeed);
-      analogWrite(motorEnBPin, motorSpeed);
+    Serial.print("LOG: Duration -> ");
+  Serial.println(duration);
+  if (distance < 20) {
+    Serial.println("ACTION: move backwards");
   } else {
-      analogWrite(motorEnAPin, 0);
-      analogWrite(motorEnBPin, 0);
+    Serial.println("ACTION: move forwards");
   }
 
 }
