c04e9bd18f4e70a646b19574ac34cd86da3c5656
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-09-17 21:43:56 +0200

    ready to test

--- brain/src/main.rs
+++ brain/src/main.rs
@@ -5,15 +5,19 @@ use std::thread::sleep;
 use std::time::Duration;
 
 fn main() {
-    let my_led = Pin::new(3); // number depends on chip, etc.
+    let motor_pin1 = Pin::new(2);
+    let motor_pin2 = Pin::new(3);
     my_led.with_exported(|| {
-        my_led.set_direction(Direction::Out).unwrap();
+        motor_pin1.set_direction(Direction::Out).unwrap();
+        motor_pin2.set_direction(Direction::Out).unwrap();
         loop {
             println!("OFF");
-            my_led.set_value(0).unwrap();
+            motor_pin1.set_value(0).unwrap();
+            motor_pin2.set_value(0).unwrap();
             sleep(Duration::from_millis(1000));
             println!("ON");
-            my_led.set_value(1).unwrap();
+            motor_pin1.set_value(1).unwrap();
+            motor_pin2.set_value(1).unwrap();
             sleep(Duration::from_millis(1000));
         }
     }).unwrap();
