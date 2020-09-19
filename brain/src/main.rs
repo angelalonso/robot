f4f0440d561464651906df8647db9bdc10992271
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-09-19 13:58:56 +0200

    ready to test

--- brain/src/main.rs
+++ brain/src/main.rs
@@ -9,11 +9,13 @@ fn main() {
     //let mut motor_a_ena = OutputDevice::new(25);
     let mut motor_a_ena = PWMOutputDevice::new(25);
 
+    println!("FORWARD");
     motor_a.forward();
     motor_a_ena.on();
     motor_a_ena.set_value(0.01);
     let sec = time::Duration::from_millis(1000);
     thread::sleep(sec);
+    println!("BACKWARD");
     motor_a_ena.set_value(0.5);
     motor_a.backward();
     thread::sleep(sec);
