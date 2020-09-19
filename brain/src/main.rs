8aad9f58c074d0d677f7dc9cc23eb51aacdc4398
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-09-19 13:46:25 +0200

    ready to test

--- brain/src/main.rs
+++ brain/src/main.rs
@@ -4,15 +4,9 @@ use std::io::prelude::*;
 
 fn main() {
     // Create a new LED attached to Pin 17
-    let mut motor_a_1 = PWMLED::new(3);
+    let mut motor_a = Motor::new(2, 3);
     let mut motor_a_ena = PWMLED::new(25);
 
-    // blink the LED 5 times
-    motor_a_1.set_blink_count(5);
-    println!("BLINKIN");
-    motor_a_1.blink(2.0, 2.0, 1.0, 1.0);
-    motor_a_ena.set_value(1000.0);
+    motor_a.forward();
 
-    // wait for key press to exit
-    let _ = io::stdin().read(&mut [0u8]).unwrap();
 }
