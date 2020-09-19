4f9c8b410569a2b3551e3dcb3f07099cfe1bbad8
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-09-19 13:58:04 +0200

    ready to test

--- brain/src/main.rs
+++ brain/src/main.rs
@@ -6,13 +6,15 @@ use std::{thread, time};
 fn main() {
     // Create a new LED attached to Pin 17
     let mut motor_a = Motor::new(2, 3);
-    let mut motor_a_ena = OutputDevice::new(25);
+    //let mut motor_a_ena = OutputDevice::new(25);
+    let mut motor_a_ena = PWMOutputDevice::new(25);
 
     motor_a.forward();
     motor_a_ena.on();
-    //motor_a_ena.set_active_high(true);
+    motor_a_ena.set_value(0.01);
     let sec = time::Duration::from_millis(1000);
     thread::sleep(sec);
+    motor_a_ena.set_value(0.5);
     motor_a.backward();
     thread::sleep(sec);
     motor_a.stop();
