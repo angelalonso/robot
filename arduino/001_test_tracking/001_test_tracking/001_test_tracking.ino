a07665cfe5a48553c51d878f58a88c387db07b9e
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-09-27 21:14:15 +0200

    ready to test

---
+++ arduino/001_test_tracking/001_test_tracking/001_test_tracking.ino
@@ -0,0 +1,25 @@
+const int TrackerPin = A5;
+int sensorValue = 0; 
+void setup() {
+  // put your setup code here, to run once
+  pinMode (TrackerPin, INPUT);
+  Serial.begin (9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("LOG: test");
+  sensorValue = analogRead (TrackerPin);
+  if (sensorValue < 50 && sensorValue < 500) 
+  {
+    delay(50);    
+    Serial.print("LOG: RESULT ON -> ");
+    Serial.println (sensorValue, DEC);
+  }
+  else (sensorValue > 500&& sensorValue > 1023);
+    {
+    delay(50);    
+    Serial.print("LOG: RESULT off -> ");
+    Serial.println (sensorValue, DEC);
+  }
+}
