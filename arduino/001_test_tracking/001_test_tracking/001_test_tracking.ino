b895f211e46480de07a71e100028f5f954c8e579
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-09-29 15:50:18 +0200

    ready to test

--- arduino/001_test_tracking/001_test_tracking/001_test_tracking.ino
+++ arduino/001_test_tracking/001_test_tracking/001_test_tracking.ino
@@ -1,25 +1,22 @@
-const int TrackerPin = A5;
+const int TrackerPin = 2;
 int sensorValue = 0; 
 void setup() {
-  // put your setup code here, to run once
   pinMode (TrackerPin, INPUT);
   Serial.begin (9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print("LOG: test");
-  sensorValue = analogRead (TrackerPin);
-  if (sensorValue < 50 && sensorValue < 500) 
-  {
+  boolean val = digitalRead(TrackerPin); // read the value of tracking module
+  if(val == HIGH) //if it is HiGH
+  { 
     delay(50);    
     Serial.print("LOG: RESULT ON -> ");
     Serial.println (sensorValue, DEC);
     delay(50);
     Serial.println("ACTION: move_forwards_slow");
   }
-  else (sensorValue > 500&& sensorValue > 1023);
-    {
+  else
+  {
     delay(50);    
     Serial.print("LOG: RESULT off -> ");
     Serial.println (sensorValue, DEC);
