0d1c4c7e1f6d285c42839a058fdcbfe327acfa31
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-09-29 18:37:07 +0200

    ready to test

--- arduino/001_test_tracking/001_test_tracking/001_test_tracking.ino
+++ arduino/001_test_tracking/001_test_tracking/001_test_tracking.ino
@@ -1,13 +1,12 @@
 const int TrackerPin = 2;
-int sensorValue = 0; 
 void setup() {
   pinMode (TrackerPin, INPUT);
   Serial.begin (9600);
 }
 
 void loop() {
-  boolean val = digitalRead(TrackerPin); // read the value of tracking module
-  if(val == HIGH) //if it is HiGH
+  boolean sensorValue = digitalRead(TrackerPin); // read the value of tracking module
+  if(sensorValue == HIGH) //if it is HiGH
   { 
     delay(50);    
     Serial.print("SENSOR: data_tracker_");
