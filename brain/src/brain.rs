0c23676be1ee4eaa90c694ac4a95128191f707ef
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-10-10 00:21:50 +0200

    ready to test

--- brain/src/brain.rs
+++ brain/src/brain.rs
@@ -122,7 +122,7 @@ impl Brain<'static> {
                 // TODO: is this needed with a ruleset?
                 let actionmsg = msg.clone();
                 let _sensormsg = msg.clone();
-                println!("TEST   {:?}", actionmsg.unwrap().split(": ")[0]);
+                println!("TEST   {:?}", actionmsg.unwrap().split(": ").collect::<Vec<_>>()[0]);
                 //if actionmsg.unwrap().split(": ").collect::<String>() == "ACTION".to_string() {
                 //    println!("got ACTION");
                 //    msg_actions.push(msg.unwrap().replace("ACTION: ", ""));
