c91e3c79889c42a4f158921c80b810364e54819f
Angel Alonso <angel.fonseca@deliveryhero.com>
2020-10-09 14:50:25 +0200

    adding move config file

--- brain/src/main.rs
+++ brain/src/main.rs
@@ -6,36 +6,42 @@ use std::env;
 
 fn show_help() {
     println!("SYNTAX: ");
-    println!(" brain <config_file> [mode]");
+    println!(" brain <actions_config_file> <auto_moves_config_file> [mode]");
     println!("");
     println!("   , where:");
     println!(" - config_file - mandatory");
-    println!("     is the path to the config yaml for triggers. ");
+    println!("     is the path to the config yaml for triggers and actions ");
+    println!(" - auto_moves_config_file - mandatory");
+    println!("     is the path to the config yaml for rules to have the robot automatically move. ");
     println!(" - mode        - optional, default is start");
     println!("     is the trigger with which the Brain starts. ");
 }
 
-fn argparser() -> (String, String) {
+fn argparser() -> (String, String, String) {
     let mut args: Vec<String> = env::args().collect();
     match args.len() {
         // No arguments passed? show error
-        1 => {
+        2 | 1 => {
             println!("ERROR: not enough parameters received");
             show_help();
             process::exit(1);
         },
         // one argument passed? then it's the config file, and mode is default one, "start"
-        2 => {
-            let ref configfile = &args[1];
-            (configfile.to_string(), "start".to_string())
+        3 => {
+            let ref brain_configfile = &args[1];
+            let ref cerebellum_configfile = &args[2];
+            (brain_configfile.to_string(), cerebellum_configfile.to_string(), "start".to_string())
         },
         // two or more argument(s) passed? join them with spaces to allow phrases
         _ => {
             // remove the prog name itself
             args.remove(0);
             // drain the config file path
-            let configfile : String = args.drain(0..1).collect();
-            (configfile.to_string(), args.join(" "))
+            let brain_configfile : String = args.drain(0..1).collect();
+            let cerebellum_configfile : String = args.drain(0..1).collect();
+            println!("--{}", brain_configfile);
+            println!("--{}", cerebellum_configfile);
+            (brain_configfile.to_string(), cerebellum_configfile.to_string(), args.join(" "))
         },
     }
 }
@@ -64,12 +70,12 @@ fn check_self_running(self_comm: &str) -> Result<(), String>{
 
 /// Load a new brain, send the first trigger, and enter the reading loop
 fn main() -> Result<(), Box<dyn Error>> {
-    let (config_file, start_mode) = argparser();
+    let (brain_config_file, _cerebellum_config_file, start_mode) = argparser();
     let args: Vec<String> = env::args().collect();
     check_self_running(&args[0]).unwrap();
     println!("Starting Brain with Mode {}", start_mode);
     // Generate our Brain object
-    let mut main_brain = Brain::new("Main Brain", config_file, None).unwrap_or_else(|err| {
+    let mut main_brain = Brain::new("Main Brain", brain_config_file, None).unwrap_or_else(|err| {
         eprintln!("Problem Initializing Main Brain: {}", err);
         process::exit(1);
     });
