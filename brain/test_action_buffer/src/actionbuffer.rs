9ea0e426be95f531844c1b6b37714c0f11887c05
Angel Alonso <angel.fonseca@deliveryhero.com>
2020-10-23 15:48:08 +0200

    threads writing and reading

--- brain/test_action_buffer/src/actionbuffer.rs
+++ brain/test_action_buffer/src/actionbuffer.rs
@@ -39,6 +39,7 @@ impl ActionBuffer {
     pub fn do_next(&mut self, timestamp: f64) -> Result<Option<Action>, String>{
         if timestamp >= self.timer {
             if self.buffer.len() == 0 {
+                self.timer = 0.0;
                 Err("No more actions to take".to_string())
             } else {
                 let a = &self.buffer.clone()[0];
@@ -50,10 +51,10 @@ impl ActionBuffer {
             Ok(None)
         }
     }
-    pub fn do_all(&mut self, start_time: f64) {
-    // TODO: run as a thread and be able to add new entries. Avoid breaking the loop
+    pub fn do_all(&mut self, start_time: f64) -> String {
         let mut latest_change = start_time;
         'outer: loop {
+        //loop {
             let ct = SystemTime::now();
             let current_time = match ct.duration_since(UNIX_EPOCH) {
                 Ok(time) => time.as_millis(),
@@ -69,10 +70,11 @@ impl ActionBuffer {
                     None => (),
                 },
                 Err(e) => {
-                    println!("{:?} - {:?}", timestamp, e);
+                    latest_change = current_time as f64;
                     break 'outer;
                 },
             };
         }
+        self.timer.to_string()
     }
 }
--- brain/test_action_buffer/src/main.rs
+++ brain/test_action_buffer/src/main.rs
@@ -1,9 +1,10 @@
 use test_action_buffer::actionbuffer::{Action, ActionBuffer};
-use std::process;
+//use std::process;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use std::thread;
 use std::sync::Arc;
 use std::sync::Mutex;
+use std::sync::mpsc::{Sender, Receiver};
 
 fn main() {
     let st = SystemTime::now();
@@ -11,55 +12,86 @@ fn main() {
         Ok(time) => time.as_millis(),
         Err(_e) => 0,
     };
-    let ab = Arc::new(Mutex::new(ActionBuffer::new().unwrap_or_else(|err| {
-                eprintln!("Problem Initializing Action Buffer: {}", err);
-                process::exit(1);
-            })));
+    let ab = Arc::new(Mutex::new(ActionBuffer::new().unwrap()));
     let a1 = Action{
         action_type: "move".to_string(),
         value: "0_0".to_string(),
-        time: 1.001,
+        time: 0.020,
     };
     let a2 = Action{
         action_type: "move".to_string(),
         value: "60_60".to_string(),
-        time: 4.361,
+        time: 0.020,
     };
     let a3 = Action{
         action_type: "move".to_string(),
         value: "-60_-60".to_string(),
-        time: 3.02,
+        time: 0.020,
+    };
+    let a4 = Action{
+        action_type: "move".to_string(),
+        value: "80_80".to_string(),
+        time: 0.020,
+    };
+    let a5 = Action{
+        action_type: "move".to_string(),
+        value: "40_40".to_string(),
+        time: 0.020,
+    };
+    let a6 = Action{
+        action_type: "move".to_string(),
+        value: "20_20".to_string(),
+        time: 0.020,
+    };
+    let a7 = Action{
+        action_type: "move".to_string(),
+        value: "44_44".to_string(),
+        time: 10.000,
     };
     ab.lock().unwrap().add(a1);
     ab.lock().unwrap().add(a2);
     ab.lock().unwrap().add(a3);
 
-    let a_b = Arc::clone(&ab);
-    let handle = thread::spawn(move || {
-        a_b.lock().unwrap().do_all(start_time as f64);
+    // ----------------------------------------------------------------------
+    
+    let (s, r): (Sender<Action>, Receiver<Action>) = std::sync::mpsc::channel();
+    let msgs = s.clone();
+    let a_b = ab.clone();
+    let hr = thread::spawn(move || {
+        loop {
+            let msg = r.recv();
+            let act = match msg.clone(){
+                Ok(a) => {
+                    println!("                                                              {:?}", a);
+                    let mut ab_r = a_b.lock().unwrap();
+                    ab_r.add(a);
+                },
+                Err(_) => (),
+            };
+        };
+    });
+
+    // TODO: test if this actually works well
+    let a_c = ab.clone();
+    let hw = thread::spawn(move || {
+        loop {
+            let mut ab_w = a_c.lock().unwrap();
+            let _r = ab_w.do_all(start_time as f64);
+        };
     });
 
     thread::sleep(Duration::from_millis(1000));
-    let a4 = Action{
-        action_type: "move".to_string(),
-        value: "80_80".to_string(),
-        time: 2.01,
-    };
-    ab.lock().unwrap().add(a4);
+    msgs.send(a4).unwrap();
     thread::sleep(Duration::from_millis(1000));
-    let a5 = Action{
-        action_type: "move".to_string(),
-        value: "40_40".to_string(),
-        time: 4.01,
-    };
-    ab.lock().unwrap().add(a5);
+    msgs.send(a5).unwrap();
     thread::sleep(Duration::from_millis(1000));
-    let a6 = Action{
-        action_type: "move".to_string(),
-        value: "20_20".to_string(),
-        time: 5.01,
-    };
-    ab.lock().unwrap().add(a6);
-
-    handle.join().unwrap();
+    msgs.send(a6).unwrap();
+    thread::spawn(move || {
+        loop {
+            thread::sleep(Duration::from_millis(3000));
+            msgs.send(a7.clone()).unwrap();
+        };
+    });
+    thread::sleep(Duration::from_millis(100));
+    hw.join().unwrap();
 }
