de7216c31514de8a067a80488967d1d6dec9191e
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-11-06 17:09:27 +0100

    latest one to test stuff

--- brain/install_and_run.sh
+++ brain/install_and_run.sh
@@ -14,5 +14,5 @@ git push origin ${DEV_BRANCH}
 
 
 ${SSH_COMM} "cd robot/brain; git pull; git checkout ${DEV_BRANCH} && git pull && \
-  RUST_LOG=info ${CARGO} run live move_cfg.yaml
+  RUST_LOG=trace ${CARGO} run live move_cfg.yaml
   "
