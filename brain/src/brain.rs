a9c33c467140f1124822f0653481fdb27568aca9
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-11-14 23:16:56 +0100

    changing and installing

--- brain/src/brain.rs
+++ brain/src/brain.rs
@@ -949,7 +949,7 @@ impl Brain {
                 let sensor = msg_parts[1].split("=").collect::<Vec<_>>();
                 let sensor_id = "arduino".to_string();
                 if sensor.len() > 1 {
-                    info!("SENSOR: {} {}", sensor[0], sensor[1]);
+                    println!("SENSOR: {} {}", sensor[0], sensor[1]);
                     self.add_metric(format!("{}__{}", sensor[0], sensor[1]), sensor_id);
                 } else {
                     println!("{:?}", sensor);
