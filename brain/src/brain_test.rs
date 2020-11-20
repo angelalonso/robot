91156fd5473d00b3b0837dd251ee4b60a95bc106
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-11-20 21:50:07 +0100

    starting test files

--- brain/src/brain_test.rs
+++ brain/src/brain_test.rs
@@ -27,7 +27,7 @@ mod brain_test {
 
         let mut test_brain = Brain::new("Test Brain".to_string(), 
                                         "dryrun".to_string(), 
-                                        "testfiles/simple_cfg.yaml".to_string()
+                                        "testfiles/simple_setup.yaml".to_string()
                                         ).unwrap_or_else(|err| {
             eprintln!("Problem Initializing Main Brain: {}", err);
             process::exit(1);
@@ -61,7 +61,41 @@ mod brain_test {
 
         let mut test_brain = Brain::new("Test Brain".to_string(), 
                                         "dryrun".to_string(), 
-                                        "testfiles/looplights_cfg.yaml".to_string()
+                                        "testfiles/looplights_setup.yaml".to_string()
+                                        ).unwrap_or_else(|err| {
+            eprintln!("Problem Initializing Main Brain: {}", err);
+            process::exit(1);
+        });
+        // loop with timestamp 
+        let (s, r): (Sender<String>, Receiver<String>) = std::sync::mpsc::channel();
+        let handle = thread::spawn(move || {
+            let _actions = test_brain.run(Some(1.8), 10, s);
+        });
+        handle.join().unwrap();
+        let mut got = [].to_vec();
+        loop {
+            match r.try_recv() {
+                Ok(m) => got.push(m),
+                Err(_) => break,
+            };
+        }
+        println!("expected: {:#x?}", expected);
+        println!("got: {:#x?}", got);
+        assert_eq!(got, expected);
+    }
+
+    #[test]
+    fn check_actions_doublefile() {
+        let expected_pointer = File::open("testfiles/doublefile_expected.yaml").unwrap();
+        let e: Vec<ActionEntry> = serde_yaml::from_reader(expected_pointer).unwrap();
+        let mut expected = [].to_vec();
+        for entry in e{
+            expected.push(format!("{:?}|{:?}", entry.time, entry.actions));
+        }
+
+        let mut test_brain = Brain::new("Test Brain".to_string(), 
+                                        "dryrun".to_string(), 
+                                        "testfiles/doublefile_setup.yaml".to_string()
                                         ).unwrap_or_else(|err| {
             eprintln!("Problem Initializing Main Brain: {}", err);
             process::exit(1);
