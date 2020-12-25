1dfb0d962cb94ba8ae2af3c376fcf839f0dd8083
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-12-25 17:17:34 +0100

    benchmarks

--- brain/benches/my_benchmark.rs
+++ brain/benches/my_benchmark.rs
@@ -7,11 +7,15 @@ extern crate criterion;
 
 use criterion::Criterion;
 
-fn actions_obstacle() {
-    let mut main_brain = Brain::new("Main Brain".to_string(), "dryrun".to_string(), "setup.yaml".to_string()).unwrap_or_else(|err| {
+fn brain_init() -> Brain {
+    let main_brain = Brain::new("Main Brain".to_string(), "dryrun".to_string(), "setup.yaml".to_string()).unwrap_or_else(|err| {
         eprintln!("Problem Initializing Main Brain: {}", err);
         process::exit(1);
     });
+    return main_brain
+}
+
+fn brain_run(mut main_brain: Brain) {
     let (s, _r): (Sender<String>, Receiver<String>) = std::sync::mpsc::channel();
     let handle = thread::spawn(move || {
         let _actions = main_brain.run(Some(2.0), 100, s);
@@ -21,8 +25,9 @@ fn actions_obstacle() {
 
 fn criterion_benchmark(c: &mut Criterion) {
     //c.bench_function("fib 20", |b| b.iter(|| fibonacci(20)));
+    let br = brain_init();
     c.sample_size(5);
-    c.bench_function("action 20", |b| b.iter(|| actions_obstacle()));
+    c.bench_function("action 20", |b| b.iter(|| brain_run(br.clone())));
 }
 
 criterion_group!(benches, criterion_benchmark);
