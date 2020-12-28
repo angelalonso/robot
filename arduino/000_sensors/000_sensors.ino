af47f9d3b87d7dc668701f8922f84de31475ff97
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2020-12-28 20:25:18 +0100

    updating and uploading

--- arduino/000_sensors/000_sensors.ino
+++ arduino/000_sensors/000_sensors.ino
@@ -1,17 +1,20 @@
-
 int incomingByte = 0;
 String msg;
 int distanceVal;
 int distancePrevVal;
+int buttonVal;
+int buttonPrevVal;
 // PINS
 const int ProximityTriggerPin = 3;
 const int ProximityEchoPin = 2;
+const int ButtonPin = 7;
 
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(ProximityTriggerPin, OUTPUT);
   pinMode(ProximityEchoPin, INPUT);
+  pinMode(ButtonPin, INPUT);
   Serial.begin(9600);
 }
 
@@ -36,10 +39,10 @@ void loop() {
   if (Serial.available() > 0) {
     // read the incoming byte:
     incomingByte = Serial.read();
-
-    // say what you got:
-    //Serial.print("I received: ");
-    //Serial.println(incomingByte, DEC);
+    
+    msg = "SENSOR: ";
+    bool news = false;
+    
     // Needed "protocol" for the proximity sensor
     digitalWrite(ProximityTriggerPin, LOW); // Reset triggerPin
     delayMicroseconds(2);
@@ -49,20 +52,27 @@ void loop() {
     long duration = pulseIn(ProximityEchoPin, HIGH);
     distanceVal = duration*0.034/2;
     //delay(30);
-    msg = "SENSOR: ";
-    bool news = false;
+
     if (distanceVal != distancePrevVal) {
       news = true;
       distancePrevVal = distanceVal;
       msg.concat("distance=");
       msg.concat(distanceVal);
       msg.concat("|");
+    
     };
+    
+    buttonVal=digitalRead(ButtonPin); //read the value of the Button
+    if (buttonVal != buttonPrevVal) {
+      news = true;
+      buttonPrevVal = buttonVal;
+      msg.concat("button=");
+      msg.concat(buttonVal);
+      msg.concat("|");
+    };
+    
     if (news == true) {
       Serial.println(msg);
-    //} else {
-    //  blink1();
-  
     }
     //delay(100);
   }
