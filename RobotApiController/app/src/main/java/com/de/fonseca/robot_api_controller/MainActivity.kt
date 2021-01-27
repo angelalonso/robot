726e9d4605960ffb4ebd1e61bb118be31029ce62
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2021-01-27 16:15:34 +0100

    app now navigates back and forth menus

--- RobotApiController/app/src/main/java/com/de/fonseca/robot_api_controller/MainActivity.kt
+++ RobotApiController/app/src/main/java/com/de/fonseca/robot_api_controller/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.de.fonseca.robot_api_controller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button_cfg)
+        button.setOnClickListener{
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     fun sendTest(view: View) {
 
     }
 }
+
---
+++ RobotApiController/app/src/main/java/com/de/fonseca/robot_api_controller/SettingsActivity.kt
@@ -0,0 +1,34 @@
+package com.de.fonseca.robot_api_controller
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+
+        val button = findViewById<Button>(R.id.button_back)
+        button.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+}
