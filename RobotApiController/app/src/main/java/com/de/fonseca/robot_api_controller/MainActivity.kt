2c0e8331cfc562a7f6b4e56bc1103320ab87e791
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2021-01-27 12:49:34 +0100

    adding app

---
+++ RobotApiController/app/src/main/java/com/de/fonseca/robot_api_controller/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.de.fonseca.robot_api_controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sendTest(view: View) {
+
+    }
+}
