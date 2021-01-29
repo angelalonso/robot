0e76147df2f2305991fc0b438ff00cb45472f36d
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2021-01-29 22:34:36 +0100

    adding an extern class

--- RobotApiController/app/src/main/java/com/de/fonseca/robot_api_controller/Globals.kt
+++ RobotApiController/app/src/main/java/com/de/fonseca/robot_api_controller/Globals.kt
@@ -1,7 +1,11 @@
 package com.de.fonseca.robot_api_controller
 
 import android.app.Application
+import android.content.Context
 import android.text.Editable
+import android.util.Log
+import android.widget.Toast
+import khttp.responses.Response
 
 class Globals : Application() {
     companion object {
@@ -16,4 +20,18 @@ class Globals : Application() {
     fun getURL(): String {
         return RobotURL;
     }
+
+
+    fun doCall(context: Context) {
+        Thread({
+            val response : Response = khttp.get("http://fconfig.co", data = "")
+            val message : String = response.text
+            Toast.makeText(context,message, Toast.LENGTH_SHORT).show()
+            Log.d("TEST", message);
+        }).start()
+
+
+    }
+
+
 }
--- RobotApiController/app/src/main/java/com/de/fonseca/robot_api_controller/MainActivity.kt
+++ RobotApiController/app/src/main/java/com/de/fonseca/robot_api_controller/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.de.fonseca.robot_api_controller
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val mContext: Context? = null
+
+    fun getContext(): Context? {
+        return mContext
+    }
+
     companion object {
         var url_list: Array<String> = emptyArray();
     }
@@ -18,10 +26,14 @@ class MainActivity : AppCompatActivity() {
 
         val debug = findViewById<TextView>(R.id.debugTv);
         debug.setText(getDebug());
-        val button = findViewById<Button>(R.id.button_cfg)
-        button.setOnClickListener{
+        val buttonCfg = findViewById<Button>(R.id.button_cfg)
+        buttonCfg.setOnClickListener{
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
+        }
+        val buttonUp = findViewById<Button>(R.id.button_up)
+        buttonUp.setOnClickListener{
+            getContext()?.let { it1 -> Globals().doCall(it1) }
         }
 
 
@@ -34,5 +46,6 @@ class MainActivity : AppCompatActivity() {
         val result = Globals().getURL();
         return result;
     }
+
 }
 
