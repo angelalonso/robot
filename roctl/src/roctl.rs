16e1eef18ae9fed6a6f956c07b15e9d4a5833ddc
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2021-03-18 21:39:37 +0100

    init creates .env file

--- roctl/src/roctl.rs
+++ roctl/src/roctl.rs
@@ -4,7 +4,7 @@ use std::fs;
 use std::fs::File;
 use std::env;
 use std::io;
-use std::io::BufReader;
+use std::io::{stdin,stdout,Write, BufReader};
 
 pub fn proxy_action(mode: String) {
     let mode_split: Vec<&str> = mode.split("_").collect();
@@ -37,15 +37,17 @@ fn init() {
 
 fn build_dot_env() -> Result<String, std::io::Error> {
     let file = ".env";
-    let envvars = ["ROBOT_IP", "SSH_CONFIG", "SSH_COMMAND", "CODE_BRANCH", "ARDUINO_FILES_PATH", "CARGO"];
+    let file_template = ".env.template";
+    let envvars = ["ROBOT_IP", "SSH_COMMAND", "SSH_CONFIG", "CODE_BRANCH", "ARDUINO_FILES_PATH", "CARGO"];
     let mut dotenv_content: String = "".to_string();
 
     // check if there is a .env file, no? -> create it
     if Path::new(file).exists() {
-        println!(".env exists");
+        println!(".env exists (Press <Enter> to keep current values)");
     } else {
-      println!(".env DOES NOT exist");
-      match File::create(file) {
+      println!(".env DOES NOT exist, using values from the template");
+      //match File::create(file) {
+      match fs::copy(format!("{}.template", file), file) {
           Ok(_) => (),
           Err(_) => panic!(),
       };
@@ -57,27 +59,44 @@ fn build_dot_env() -> Result<String, std::io::Error> {
     //   ask user showing current value as default
     //   once all vars have a value, write them to .env
     for envvar in envvars.iter() {
-        println!("checking {}...", envvar);
         let mut fenv = File::open(file)?;
         let mut reader = BufReader::new(fenv);
-        let mut envvar_found = false;
+        let mut envvar_found = "".to_string();
         for line_opt in reader.lines() {
             match line_opt {
-                Ok(l) => {
-                    if l.contains(envvar) {
-                        println!("fooooooound!");
-                        envvar_found = true;
-                    };
+                Ok(l) => { 
+                    let l_keyval: Vec<&str> = l.split(":").collect();
+                    if l_keyval[0].contains(envvar) {envvar_found = l }
                 },
-                Err(_) => ()
+                Err(_) => (),
             }
         };
-        if !envvar_found {dotenv_content.push_str(&format!("{}: null\n", envvar));}
+        dotenv_content.push_str(&get_new_envvar(envvar.to_string(), envvar_found.clone()));
     };
-    println!("{}", dotenv_content);
-    let mut f = File::open(file)?;
+    //println!("{}", dotenv_content);
+    let mut f = File::create(file)?;
     f.write_all(dotenv_content.as_bytes()).expect("Unable to write data");
     Ok("".to_string())
+}
+
+fn get_new_envvar(entry: String, previous_entry: String) -> String {
+    let mut s = String::new();
+    // get previous value
+    let prev_keyval: Vec<&str> = previous_entry.split(":").collect();
+    print!("Please enter a value for {}: [{}] ", entry, prev_keyval[1]);
+    let _=stdout().flush();
+    stdin().read_line(&mut s).expect("Did not enter a correct string");
+    if let Some('\n') = s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r') = s.chars().next_back() {
+        s.pop();
+    }
+    if s == "" {s.push_str(prev_keyval[1])};
+    if s.chars().nth(0).unwrap() == ' ' {s.remove(0);};
+    println!("Added: '{}: {}'", entry, s);
+
+    return format!("{}: {}\n", entry, s)
 }
 
 fn get_stuff(what: String) {
