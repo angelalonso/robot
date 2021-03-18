5d0d87aa4e09730f76f19d18ada973c530e25d9e
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2021-03-18 21:44:07 +0100

    dotenv ready

--- roctl/src/roctl.rs
+++ roctl/src/roctl.rs
@@ -35,29 +35,24 @@ fn init() {
     build_dot_env();
 }
 
+/// Checks previous values at .env
+///   if .env doesn't exist, it gets copied from the template file
+///   after that, the user gets asked for each envvar's value
+///     using the previous value as default one
 fn build_dot_env() -> Result<String, std::io::Error> {
     let file = ".env";
     let file_template = ".env.template";
     let envvars = ["ROBOT_IP", "SSH_COMMAND", "SSH_CONFIG", "CODE_BRANCH", "ARDUINO_FILES_PATH", "CARGO"];
     let mut dotenv_content: String = "".to_string();
-
-    // check if there is a .env file, no? -> create it
     if Path::new(file).exists() {
         println!(".env exists (Press <Enter> to keep current values)");
     } else {
       println!(".env DOES NOT exist, using values from the template");
-      //match File::create(file) {
       match fs::copy(format!("{}.template", file), file) {
           Ok(_) => (),
           Err(_) => panic!(),
       };
     }
-
-    // TODO
-    // go over envvars, 
-    //   get current value from .env, 
-    //   ask user showing current value as default
-    //   once all vars have a value, write them to .env
     for envvar in envvars.iter() {
         let mut fenv = File::open(file)?;
         let mut reader = BufReader::new(fenv);
@@ -79,9 +74,11 @@ fn build_dot_env() -> Result<String, std::io::Error> {
     Ok("".to_string())
 }
 
+/// Provided a key and a previous key-val pair(in a "key: val" format),
+///   we ask the user for the new val
+///   using the previous one as default
 fn get_new_envvar(entry: String, previous_entry: String) -> String {
     let mut s = String::new();
-    // get previous value
     let prev_keyval: Vec<&str> = previous_entry.split(":").collect();
     print!("Please enter a value for {}: [{}] ", entry, prev_keyval[1]);
     let _=stdout().flush();
@@ -95,7 +92,6 @@ fn get_new_envvar(entry: String, previous_entry: String) -> String {
     if s == "" {s.push_str(prev_keyval[1])};
     if s.chars().nth(0).unwrap() == ' ' {s.remove(0);};
     println!("Added: '{}: {}'", entry, s);
-
     return format!("{}: {}\n", entry, s)
 }
 
