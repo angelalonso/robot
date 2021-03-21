537e35e76c02f3f3caff1eb16520852f70966dfd
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2021-03-21 15:25:33 +0100

    roctl compile and git commands ready

--- roctl/src/dostuff.rs
+++ roctl/src/dostuff.rs
@@ -1,53 +1,87 @@
 use std::process::Command;
-
-static BRAIN_DIR: &str = "../brain";
+use std::env;
 
 pub fn run() {
-    compile();
+    if compile() {
+        println!("Compilation OK");
+        if git_push() {
+            println!("Pushing changes to branch OK");
+            if reset() {
+                println!("Resetting Robot OK");
+            };
+        };
+    };
 //  doCompile $@
-//  TITLE="ITEM    RUN"
-//  echo "${TITLE}"
-//  TITLE_SHOWN="True"
-//  if [[ "${RESULT}" != "Robot   True" ]]; then
-//    RESULT="Robot   Error: Compilation failed"
-//  else
-//    RESULT="Robot   OK: Compilation"
-//    echo -e "${RESULT}"
 //    doGitPush
-//    if [[ "${RESULT}" != "Robot   True" ]]; then
-//      RESULT="Robot   Error: Updating Git branch failed"
-//    else
-//      RESULT="Robot   OK: Updating git branch"
-//      echo -e "${RESULT}"
 //      doReset
-//      if [[ "${RESULT}" != "Robot   True" ]]; then
-//        RESULT="Robot   Error: Reset failed"
-//      else
-//        RESULT="Robot   OK: Resetting"
-//        echo -e "${RESULT}"
-//        RESULT="Robot   Run ..."
-//        echo -e "${RESULT}"
 //        ${SSH_COMM} "cd robot/brain; git pull; git checkout ${DEV_BRANCH} && git stash; git stash drop; git pull && \
-//          RUST_LOG=info target/arm-unknown-linux-gnueabihf/debug/brain live setup.yaml
-//          "
-//        EXIT=$?
-//        if [[ $EXIT -eq 0 ]]; then
-//          RESULT="Robot   True"
-//        else
-//          RESULT="Robot   Error: "$EXIT
-//        fi
-//      fi
-//    fi
-//  fi
 }
 
-fn compile() {
-    let output = Command::new("cross")
-        .arg("build")
-        .arg("--target=arm-unknown-linux-gnueabihf")
-        .current_dir(BRAIN_DIR)
-        .output()
+fn compile() -> bool {
+    let cwd: String = env::current_dir().unwrap().into_os_string().into_string().unwrap();
+    let brain_dir: &str = &format!("{}/../brain/", cwd);
+    // TODO: avoid using a script to do this. Is it possible even?
+    let comm_status = Command::new("./roctl_do_compile.sh")
+        .current_dir(brain_dir)
+        .status()
         .expect("cross compile command failed to start");
-    let stdout = String::from_utf8(output.stdout).unwrap();
-    println!("{}", stdout);
+    if comm_status.success() {
+        return true
+            
+    } else {
+        return false
+    }
+}
+
+fn git_push() -> bool {
+    let cwd: String = env::current_dir().unwrap().into_os_string().into_string().unwrap();
+    let brain_dir: &str = &format!("{}/../brain/", cwd);
+    // TODO: use env vars to get branch 
+    let gcheckout_status = Command::new("git")
+        .arg("checkout")
+        .arg("dev002")
+        .current_dir(brain_dir)
+        .status()
+        .expect("git checkout command failed to start");
+    if gcheckout_status.success() {
+        let gadd_status = Command::new("git")
+            .arg("add")
+            .arg(".")
+            .current_dir(brain_dir)
+            .status()
+            .expect("git add command failed to start");
+        if gadd_status.success() {
+            let gcommit_status = Command::new("git")
+                .arg("commit")
+                .arg("-m")
+                .arg("\"auto commit by roctl\"")
+                .current_dir(brain_dir)
+                .status()
+                .expect("git commit command failed to start");
+            if gcommit_status.success() {
+                let gpush_status = Command::new("git")
+                    .arg("push")
+                    .arg("origin")
+                    .arg("dev002")
+                    .current_dir(brain_dir)
+                    .status()
+                    .expect("git push command failed to start");
+                if gpush_status.success() {
+                    return true
+                } else {
+                    return false
+                }
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
+    } else {
+        return false
+    }
+}
+
+fn reset() -> bool {
+    true
 }
