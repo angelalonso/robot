8c00d5536ac75170d3aa1bd2b09286483dc054db
Angel Alonso <alonsofonseca.angel@gmail.com>
2021-08-30 17:48:42 +0200

    first real actions

---
+++ ros/brain/scripts/led21off_action_server.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.action import ActionServer
+from rclpy.node import Node
+#import RPi.GPIO as GPIO
+from fake_rpi import fake_rpi as GPIO
+
+from brain.action import Led21ffO
+
+
+class Led21OffActionServer(Node):
+
+    def __init__(self):
+        super().__init__('led21off_action_server')
+        GPIO.setmode(GPIO.BCM)
+        self.pin_id = 21
+        self._action_server = ActionServer(
+            self,
+            Led21Off,
+            'Led21Off',
+            self.execute_callback)
+
+    def execute_callback(self, goal_handle):
+        self.get_logger().info('Executing goal...')
+
+        feedback_msg = Led21Off.Feedback()
+        feedback_msg.process_feed = "handling PIN " + str(self.pin_id)
+        self.get_logger().info('Feedback: {}'.format(feedback_msg.process_feed))
+        goal_handle.publish_feedback(feedback_msg)
+
+        GPIO.setup(self.pin_id, GPIO.OUT)
+        GPIO.output(self.pin_id, GPIO.HIGH)
+        goal_handle.succeed()
+        result = Led21Off.Result()
+        return result
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    led21off_action_server = Led21OffActionServer()
+
+    rclpy.spin(led21off_action_server)
+
+
+if __name__ == '__main__':
+    main()
---
+++ ros/brain/scripts/led21on_action_server.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.action import ActionServer
+from rclpy.node import Node
+#import RPi.GPIO as GPIO
+from fake_rpi import fake_rpi as GPIO
+
+from brain.action import Led21On
+
+
+class Led21OnActionServer(Node):
+
+    def __init__(self):
+        super().__init__('led21on_action_server')
+        GPIO.setmode(GPIO.BCM)
+        self.pin_id = 21
+        self._action_server = ActionServer(
+            self,
+            Led21On,
+            'Led21On',
+            self.execute_callback)
+
+    def execute_callback(self, goal_handle):
+        self.get_logger().info('Executing goal...')
+
+        feedback_msg = Led21On.Feedback()
+        feedback_msg.process_feed = "handling PIN " + str(self.pin_id)
+        self.get_logger().info('Feedback: {}'.format(feedback_msg.process_feed))
+        goal_handle.publish_feedback(feedback_msg)
+
+        GPIO.setup(self.pin_id, GPIO.OUT)
+        GPIO.output(self.pin_id, GPIO.HIGH)
+        goal_handle.succeed()
+        result = Led21On.Result()
+        return result
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    led21on_action_server = Led21OnActionServer()
+
+    rclpy.spin(led21on_action_server)
+
+
+if __name__ == '__main__':
+    main()
