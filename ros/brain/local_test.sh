818173a21538cef1cd71a2d682e8fdd9288dc972
Angel Alonso <alonsofonseca.angel@gmail.com>
2021-09-04 16:03:32 +0200

    local development script

--- ros/brain/local_test.sh
+++ ros/brain/local_test.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env zsh
 
+function ctrl_c() {
+  # once the launch run is stopped with CTRL-C we want to clean up
+  echo "** Trapped CTRL-C"
+  # Set back to real libraries to have them ready to be pushed to the repo
+  sed -i 's/^#import RPi.GPIO as GPIO/import RPi.GPIO as GPIO/g' scripts/led21on_action_server.py
+  sed -i 's/^from fake_rpi import fake_rpi as GPIO/#from fake_rpi import fake_rpi as GPIO/g' scripts/led21on_action_server.py
+}
+
+# Set fake libraries for laptop development
+sed -i 's/^import RPi.GPIO as GPIO/#import RPi.GPIO as GPIO/g' scripts/led21on_action_server.py
+sed -i 's/^#from fake_rpi import fake_rpi as GPIO/from fake_rpi import fake_rpi as GPIO/g' scripts/led21on_action_server.py
+
+trap ctrl_c INT
+
 colcon build && \
   . ./install/setup.zsh && \
   ros2 launch brain brain.launch.py
+
