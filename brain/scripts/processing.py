563faf7e5c206693297e1cc35d4ac45ec7b14685
Angel Alonso <alonsofonseca.angel@gmail.com>
2021-10-10 09:09:32 +0200

    controlled mocked distance, transformed to actions

--- brain/scripts/processing.py
+++ brain/scripts/processing.py
@@ -19,9 +19,9 @@ def understand(data):
                 topic = info.split(': ')[1].split('=')[0]
                 try:
                     value = info.split(': ')[1].split('=')[1]
-                    #TODO: check source is correct, do different stuff for different sources
-                    #TODO: check topic is correct, do different stuff for different topics
-                    if (source == 'SENSOR') and (topic == 'distance') and (value != ''):
+                    #TODO: do different stuff for different sources
+                    #TODO: do different stuff for different topics
+                    if (source == 'SENSOR') and (topic == 'distance') and (value.isnumeric()):
                         info_list_clean.append(info_entry(source, topic, value))
                 except IndexError:
                     pass
@@ -35,7 +35,16 @@ def get_actions(data):
     info_list = understand(data)
     action_list = []
     for entry in info_list:
-        action_list.append(str(entry))
+        #TODO: play with values, past values etc...to do more complex actions
+        if int(entry.value) < 20:
+            action_list.append("MOTOR: left=-100")
+            action_list.append("MOTOR: right=-100")
+        elif int(entry.value) < 30:
+            action_list.append("MOTOR: left=-100")
+            action_list.append("MOTOR: right=100")
+        else:
+            action_list.append("MOTOR: left=100")
+            action_list.append("MOTOR: right=100")
     return '*'.join(action_list)
 
 def process_input(data):
