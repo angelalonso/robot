529d093226d95b9621dcab2d2e563128d45b4281
Angel Alonso <alonsofonseca.angel@gmail.com>
2021-10-25 18:19:19 +0200

    function to read ruleset

--- brain/scripts/python_test/test.py
+++ brain/scripts/python_test/test.py
@@ -1,13 +1,51 @@
-from pandas import read_csv
-# Load dataset
-url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
-names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-dataset = read_csv(url, names=names)
-# shape
-print(dataset.shape)
-# head
-print(dataset.head(20))
-# descriptions
-print(dataset.describe())
-# class distribution
-print(dataset.groupby('class').size())
+import csv
+import sys
+
+def read_rules(filename):
+    csvfile = open(filename, newline='')
+    dictreader = csv.DictReader(csvfile, delimiter=';')
+    return dictreader
+
+def find_match(dataset, new_input):
+    # TODO: open this up for more than one input variable
+    prev_entry = None
+    prev_diff = None
+    next_entry = None
+    next_diff = None
+    for entry in dataset:
+    # If there's a direct match, get the result
+        if entry['distance'] == new_input:
+            return entry['speed']
+    # If there isn't a direct match, find the prev and next values, 
+    #   if they have the same result, that's our return
+    #   if not, choose the prev over the next
+        else:
+            diff = int(entry['distance']) - int(new_input)
+            if diff < 0:
+                if prev_diff == None:
+                    prev_diff = diff
+                    prev_entry = entry
+                elif diff > prev_diff:
+                    prev_diff = diff
+                    prev_entry = entry
+
+            elif diff > 0:
+                if next_diff == None:
+                    next_diff = diff
+                    next_entry = entry
+                elif diff < next_diff:
+                    next_diff = diff
+                    next_entry = entry
+    if prev_entry == None:
+        if next_entry == None:
+            return None
+        else:
+            return next_entry['speed']
+    else:
+        return prev_entry['speed']
+    
+if __name__ == '__main__':
+    data = read_rules('rules.csv')
+    print(data.fieldnames[-1])
+    print(find_match(data, sys.argv[1]))
+
