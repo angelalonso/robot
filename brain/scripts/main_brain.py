284099dc5012ba2e74181dd65ed94bf131094908
Angel Alonso <alonsofonseca.angel@gmail.com>
2021-12-20 18:40:26 +0100

    adding actionsets

--- brain/scripts/main_brain.py
+++ brain/scripts/main_brain.py
@@ -3,15 +3,22 @@
 from rclpy import init, logging, spin, shutdown
 from rclpy.node import Node
 
+import yaml
+
 class TimedActions(Node):
 
     def __init__(self):
         super().__init__('timed_actions')
         self.get_logger().info('OK ')
         # load set of actions' definitions
+        self.load_actionsets()
         # loop that applies actions when:
         # - time is equal of bigger than defined
         # - previous action-set is finished AND it is set to repeat (more than 0 times, or forever)
+
+    def load_actionsets(self):
+        self.actionsets = yaml.load(open('actionsets/actionset.yaml'))
+        #self.get_logger().info('%s' % self.actionsets[0]['name'])
 
 def main(args=None):
     init(args=args)
