12fc46173615b0900bdf6d160a4eb97ded4d323c
Angel Alonso <alonsofonseca.angel@gmail.com>
2021-12-20 22:59:02 +0100

    triggering actions

--- brain/scripts/main_brain.py
+++ brain/scripts/main_brain.py
@@ -3,13 +3,14 @@
 from rclpy import init, logging, spin, shutdown
 from rclpy.node import Node
 
+from datetime import datetime
 import yaml
 
 class TimedActions(Node):
 
     def __init__(self):
         super().__init__('timed_actions')
-        self.get_logger().info('OK ')
+        self.starttime = datetime.now()
         # load set of actions' definitions
         self.load_actionsets()
         # loop that applies actions when:
@@ -20,14 +21,26 @@ class TimedActions(Node):
         self.actionsets = yaml.load(open('actionsets/actionset.yaml'))
         #self.get_logger().info('%s' % self.actionsets[0]['name'])
 
+    def trigger_actions(self):
+        # todo: distinguish between triggered and untriggered ones
+        while True:
+            current_raw = datetime.now() - self.starttime
+            current = current_raw.seconds + (current_raw.microseconds / 1000000)
+            for action in self.actionsets:
+                if (action['starts_at'] + action['starts_after']) <= current:
+                    self.get_logger().info('%s' % current)
+                    self.get_logger().info('%s' % action['actions'][0])
+
+
+
+
 def main(args=None):
     init(args=args)
 
     timed_actions_node = TimedActions()
 
-    spin(timed_actions_node)
+    timed_actions_node.trigger_actions()
 
-    timed_actions_node.destroy_node()
     shutdown()
 
 
