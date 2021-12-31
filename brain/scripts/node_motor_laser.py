548e8c07dccca4a7df4b686def0aeea28e243d2e
Angel Alonso <alonsofonseca.angel@gmail.com>
2021-12-31 19:04:57 +0100

    laser motor test2

--- brain/scripts/node_motor_laser.py
+++ brain/scripts/node_motor_laser.py
@@ -6,25 +6,27 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+correction = -1.25
+p.start(2.5 + correction) # Initialization
 try:
   while True:
-    p.ChangeDutyCycle(5)
+    p.ChangeDutyCycle(5 + correction)
     time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
+    p.ChangeDutyCycle(7.5 + correction)
     time.sleep(0.5)
-    p.ChangeDutyCycle(10)
+    p.ChangeDutyCycle(10 + correction)
     time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
+    p.ChangeDutyCycle(12.5 + correction)
     time.sleep(0.5)
-    p.ChangeDutyCycle(10)
+    p.ChangeDutyCycle(10 + correction)
     time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
+    p.ChangeDutyCycle(7.5 + correction)
     time.sleep(0.5)
-    p.ChangeDutyCycle(5)
+    p.ChangeDutyCycle(5 + correction)
     time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
+    p.ChangeDutyCycle(2.5 + correction)
     time.sleep(0.5)
 except KeyboardInterrupt:
   p.stop()
   GPIO.cleanup()
+
