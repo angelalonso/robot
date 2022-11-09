1b51acd38da0e8133b11fc7164f345d6b90e87a6
Angel Alonso <alonsofonseca.angel@gmail.com>
2022-11-09 21:23:05 +0100

    adding action servers

--- circuits/build.sh
+++ circuits/build.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
+if [[ $1 == "" ]]; then
+  source /opt/ros/rolling/setup.bash
 
-source /opt/ros/rolling/setup.bash
+  export ROS_DOMAIN_ID=4
+  export ROS_LOCALHOST_ONLY=1
 
-export ROS_DOMAIN_ID=4
-export ROS_LOCALHOST_ONLY=1
+  colcon build
 
-colcon build
+  . ./install/local_setup.bash
 
-. ./install/local_setup.bash
-
-ros2 run circuit_nodes led_action_server &
-ros2 run circuit_nodes node_master &
+  ros2 run circuit_nodes led_action_server &
+  ros2 run circuit_nodes node_master &
+fi
+if [[ $1 == "off" ]]; then
+  kill $(ps aux | grep robot | grep circuits | awk '{print $2}')
+fi
--- circuits/src/circuit_nodes/src/node_master.cpp
+++ circuits/src/circuit_nodes/src/node_master.cpp
@@ -1,26 +1,76 @@
+#include <chrono>
+#include <cinttypes>
+
+#include "action_interfaces/action/led.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
 
-class MasterNode : public rclcpp::Node
+using Led = action_interfaces::action::Led;
+
+
+int main(int argc, char ** argv)
 {
-public:
-  MasterNode() : Node("master_node") {
-    timer_ = this->create_wall_timer(
-      std::chrono::milliseconds(200),
-      std::bind(&MasterNode::timerCallback, this)
-    );
-
-  }
-private:
-  void timerCallback() {
-    RCLCPP_INFO(this->get_logger(), "Hello from ROS2");
-  }
-  rclcpp::TimerBase::SharedPtr timer_;
-};
-
-int main(int argc, char * argv[]) {
   rclcpp::init(argc, argv);
-  auto node = std::make_shared<MasterNode>();
-  rclcpp::spin(node);
+  auto node = rclcpp::Node::make_shared("minimal_action_client");
+  auto action_client = rclcpp_action::create_client<Led>(node, "led");
+
+  if (!action_client->wait_for_action_server(std::chrono::seconds(20))) {
+    RCLCPP_ERROR(node->get_logger(), "Action server not available after waiting");
+    return 1;
+  }
+
+  // Populate a goal
+  auto goal_msg = Led::Goal();
+  goal_msg.turn_on = 10;
+
+  RCLCPP_INFO(node->get_logger(), "Sending goal");
+  // Ask server to achieve some goal and wait until it's accepted
+  auto goal_handle_future = action_client->async_send_goal(goal_msg);
+  if (rclcpp::spin_until_future_complete(node, goal_handle_future) !=
+    rclcpp::FutureReturnCode::SUCCESS)
+  {
+    RCLCPP_ERROR(node->get_logger(), "send goal call failed :(");
+    return 1;
+  }
+
+  rclcpp_action::ClientGoalHandle<Led>::SharedPtr goal_handle = goal_handle_future.get();
+  if (!goal_handle) {
+    RCLCPP_ERROR(node->get_logger(), "Goal was rejected by server");
+    return 1;
+  }
+
+  // Wait for the server to be done with the goal
+  auto result_future = action_client->async_get_result(goal_handle);
+
+  RCLCPP_INFO(node->get_logger(), "Waiting for result");
+  if (rclcpp::spin_until_future_complete(node, result_future) !=
+    rclcpp::FutureReturnCode::SUCCESS)
+  {
+    RCLCPP_ERROR(node->get_logger(), "get result call failed :(");
+    return 1;
+  }
+
+  rclcpp_action::ClientGoalHandle<Led>::WrappedResult wrapped_result = result_future.get();
+
+  switch (wrapped_result.code) {
+    case rclcpp_action::ResultCode::SUCCEEDED:
+      break;
+    case rclcpp_action::ResultCode::ABORTED:
+      RCLCPP_ERROR(node->get_logger(), "Goal was aborted");
+      return 1;
+    case rclcpp_action::ResultCode::CANCELED:
+      RCLCPP_ERROR(node->get_logger(), "Goal was canceled");
+      return 1;
+    default:
+      RCLCPP_ERROR(node->get_logger(), "Unknown result code");
+      return 1;
+  }
+
+  RCLCPP_INFO(node->get_logger(), "result received");
+  for (auto number : wrapped_result.result->confirmed) {
+    RCLCPP_INFO(node->get_logger(), "%" PRId32, number);
+  }
+
   rclcpp::shutdown();
   return 0;
 }
